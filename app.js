a53ad0124248c3e1291a6f2a61ca087408823e8e
Luisa <luisa2000@gmail.com>
2014-09-25 15:24:40 -0400

    Stopped recreating the app object

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
+//var path = require('path');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 //Database
@@ -10,27 +10,25 @@ var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk("localhost:27017/ThreadSwap");
 
-var routes = require('./routes/index');
+
 
 var app = express();
 
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
 
-// Make our db accessible to our router
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
+var routes = require('./routes/index');
+routes(app, db);
 
 app.use('/', routes);
 
@@ -64,6 +62,7 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,83 +1,81 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(app, db) {
 
-var dbCollection = "swapcollection";
-/* GET home page. */
-router.get('/', function(req, res) {
-	res.sendfile('./public/views/index.html'); // load our public/index.html file
-	console.log("Router index page");
-});
-
-router.get('/swaps', function(req, res) {
-	console.log("GET");
-	var db = req.db;
-	var Swaps = db.get(dbCollection);
-	//console.log(dbCollection);
-	var result = Swaps.find({}, {fields: {ImageURL: 1}}, function(err, swaps) {
-		//console.log(swaps);
-		if (err)
-			res.send(err);
-		res.json(swaps); //return all swaps in json format
+	//var router = app.Router();
+	var dbCollection = "swapcollection";
+	/* GET home page. */
+	app.get('/', function(req, res) {
+		res.sendfile('./public/views/index.html'); // load our public/index.html file
+		console.log("Router index page");
 	});
-	//console.log(result);
-	console.log("GET bottom");
-});
 
-router.get('/swaps/:swapid', function(req, res) {
-	console.log("GETBYID");
-	console.log("swapId");
-	var db = req.db;
-	var Swaps = db.get(dbCollection);
-	//Get full object by ID
-	Swaps.find({_id:req.params.swapid.toString()},{},function(err, swaps) {
-		console.log(swaps+"    swaps");
-		if (err)
-			res.send(err);
-		res.json(swaps); //return swap obj in json format
+	app.get('/swaps', function(req, res) {
+		console.log("GET");
+		var Swaps = db.get(dbCollection);
+		var result = Swaps.find({}, {fields: {ImageURL: 1}}, function(err, swaps) {
+			if (err)
+				res.send(err);
+			res.json(swaps); //return all swaps in json format
+		});
+		console.log("GET bottom");
 	});
-	console.log("GETBYID bottom");
-});
 
-router.post('/swaps', function(req, res) {
-	console.log("POST");
-	console.dir(req.body + "req body");
-	var igLink = req.body.InstagramLink == null ? null : req.body.InstagramLink;
-	var swapObj = {
-		"Title":req.body.SwapTitle, 
-		"Seller": req.body.Seller,
-		"Latitude": req.body.lat,
-		"Longitude": req.body.lng,
-		"Price": req.body.Price,
-		"Size": req.body.Size,
-		"ImageURL": req.body.ImageURL,
-		"Description": req.body.Description,
-		"InstagramLink": igLink,
-		"ContactInfo": req.body.ContactInfo
-	};
-	//validation of obj
-	var db = req.db;
-	var Swaps = db.get(dbCollection);
-	Swaps.insert(swapObj, function(err, doc) {
-		if (err) {
-			res.send("There was a problem adding the information to the database");
-		}
-		else {
-			res.send(doc);
-		}
+	app.get('/swaps/:swapid', function(req, res) {
+		console.log("GETBYID");
+		var Swaps = db.get(dbCollection);
+		//Get full object by ID
+		Swaps.find({_id:req.params.swapid.toString()},{},function(err, swaps) {
+			console.log(swaps+"    swaps");
+			if (err)
+				res.send(err);
+			res.json(swaps); //return swap obj in json format
+		});
+		console.log("GETBYID bottom");
 	});
-	console.log("POST bottom");
-});
 
-router.delete('/swaps/:swapid', function(req, res) {
-	console.log("DELBYID");
-	var swapId = req.params.swapid;
+	app.post('/swaps', function(req, res) {
+		console.log("POST");
+		console.dir(req.headers+"  req headers");
+		console.dir(req.body + "   req body");
 
-	var db = req.db;
-	var Swaps = db.get(dbCollection);
-	//Get full object by ID
-	Swaps.remove({_id : swapId});
-	console.log("DELBYID bottom");
-});
+		var igLink = req.body.InstagramLink == null ? null : req.body.InstagramLink;
+		var swapObj = {
+			"Title":req.body.Title, 
+			"Seller": req.body.Seller,
+			"Latitude": req.body.lat,
+			"Longitude": req.body.lng,
+			"Price": req.body.Price,
+			"Size": req.body.Size,
+			"ImageURL": req.body.ImageURL,
+			"Description": req.body.Description,
+			"InstagramLink": igLink,
+			"ContactInfo": req.body.ContactInfo
+		};
+		console.log(swapObj);
+		//validation of obj
+		var Swaps = db.get(dbCollection);
+		Swaps.insert(swapObj, function(err, doc) {
+			if (err) {
+				res.send("There was a problem adding the information to the database");
+			}
+			else {
+				res.send(doc);
+			}
+		});
+		console.log("POST bottom");
+	});
 
+	app.delete('/swaps/:swapid', function(req, res) {
+		console.log("DELBYID");
+		var swapId = req.params.swapid;
 
-module.exports = router;
+		var Swaps = db.get(dbCollection);
+		console.log("debug1");
+		//Get full object by ID
+		Swaps.remove({_id : swapId});
+		console.log("DELBYID bottom");
+		res.end();
+	});
+
+	//app.use('/', router);
+}
+//module.exports = router;
